1934afc3e62d477104403a985bf2480afe15cfd3
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-28 13:45:30 -0400

    changed routes filestructure

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+const path = require('path');
 
 router.get('/exercise', (req, res) => {
+  console.log('HTML /GET EXERCISE');
     res.sendFile(path.join(__dirname, '../public/exercise.html' ));
 });
 
 router.get("/stats", (req, res) => {
+   console.log("HTML /GET STATS");
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
