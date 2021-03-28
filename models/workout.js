60a4b921e4bee573e1dbcf33bd20eef1c91abb6d
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-28 13:12:16 -0400

    trying to seed and need help

--- models/workout.js
+++ models/workout.js
@@ -35,6 +35,6 @@ const workoutSchema = new Schema({
   ],
 });
 
-const Workout = mongoose.model("Workout", workSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
