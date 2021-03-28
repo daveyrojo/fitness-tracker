a89218f90e887585e2776379bc346f21bce9d4da
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-28 11:46:20 -0400

    added post and delete route

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,22 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 const workout = require("../models/workout.js");
 
-router.post("/api/workout.js", ({ body }, res) => {
+router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
-    .then((dbTransaction) => {
+    .then((dbWorkout) => {
         res.json(dbWorkout);
     })
     .catch((err) => {
         res.status(400).json(err);
     })
-})
+});
+
+router.delete("/api/workouts/", ({ body}, res) => {
+    Workout.findByIdAndDelete(body.id)
+    .then(() => {
+        return res.json(true);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
+});
