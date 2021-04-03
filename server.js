4a70a5b1135293fecbd5eb621ce7794ae6269358
David Eldridge <d.vincenteldridge@gmail.com>
2021-04-03 01:21:31 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const path = require('path');
+const mongoose = require("mongoose");
+const express = require("express");
+const path = require("path");
 const PORT = 3000;
 
 const app = express();
@@ -10,10 +10,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
-    console.log(`HOLY COW YOU DID IT! ${PORT} BEING USED`);
+  console.log(`HOLY COW YOU DID IT! ${PORT} BEING USED`);
 });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewURlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    }
+);
 
 app.use(require("./routes/api"));
 app.use(require("./routes/htmlroutes"));
